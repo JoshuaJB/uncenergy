4d9f801754f6b242a1fc20d60ec0dd429e4be276
Joshua Bakita <bakitajoshua@gmail.com>
2014-10-26 04:46:32 -0400

    Fix, fix, fix

--- api.php
+++ api.php
@@ -1,6 +1,6 @@
 <?php
 
 $building = $_REQUEST['building'];
-$response = file_get_contents('https://itsapps.unc.edu/energy/rest/buildings/' + $building);
+$response = file_get_contents('https://itsapps.unc.edu/energy/rest/buildings/' . $building);
 header('Content-Type: application/json');
 echo json_encode($response);
