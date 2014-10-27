9db7b6ba4e9bcd83f50704e0e43c42a6c57fc18d
Joshua Bakita <bakitajoshua@gmail.com>
2014-10-27 17:11:31 -0400

    Upgrades to the PHP script

--- api.php
+++ api.php
@@ -1,7 +1,28 @@
 <?php
 
-$building = $_REQUEST['building'];
-$json = file_get_contents('https://itsapps.unc.edu/energy/rest/buildings/' . $building);
-$response = json_decode($json);
+// Empty fallthrough response
+$response = jason_encode("{}");
+
+switch(array_keys($_REQUEST)[0])
+{
+	case 'building':
+		if ($_REQUEST['building'] > 0 && $_REQUEST['building'] < 1000)
+		{
+			if (array_key_exists('live', $_REQUEST) && $_REQUEST['live'] == true)
+				// Only live data
+				$response = file_get_contents('https://itsapps.unc.edu/energy/rest/buildings/' . $_REQUEST['building'] . '/live');
+			else
+				// Live and historical data
+				$response = file_get_contents('https://itsapps.unc.edu/energy/rest/buildings/' . $_REQUEST['building']);
+		}
+		break;
+	case 'campus':
+		// Aggregate data
+		$response = file_get_contents('https://itsapps.unc.edu/energy/rest/campus');
+		break;
+}
+// Return
 header('Content-Type: application/json');
-echo json_encode($response);
+echo($response);
+
+?>
