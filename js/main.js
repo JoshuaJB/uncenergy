eeb0fbe8260733b8ba1408f298076daf622096d0
Joshua Bakita <bakitajoshua@gmail.com>
2014-11-08 12:45:00 -0800

    Try to get back building names

--- js/main.js
+++ js/main.js
@@ -5,6 +5,12 @@ google.setOnLoadCallback(forceUpdate);
 var currentBuilding = '113';
 var energyType = 'electricity';
 var historyType = 'daily';
+var buildingNameMap = {};
+
+new JSONHttpRequest('/buildingmap.json',
+					function(result) {buildingNameMap = result;},
+					function() {showError("Unable to load building list. Please refresh.");}
+					);
 
 function JSONHttpRequest(URL, loadCallback, errorCallback)
 {
@@ -14,16 +20,18 @@ function JSONHttpRequest(URL, loadCallback, errorCallback)
 	this.onRequestComplete = function () {
 		try
 		{
-			_this.loadCallback(JSON.parse(_this.httpRequest.responseText));
+			response = JSON.parse(_this.httpRequest.responseText);
 		}
 		catch(SyntaxError)
 		{
-			_this.errorCallback();
+			_this.errorCallback("Unable to parse server response.");
+			return;
 		}
+		_this.loadCallback(response);
 	};
 	this.httpRequest = new XMLHttpRequest();
 	this.httpRequest.addEventListener("load", this.onRequestComplete, false);
-	this.httpRequest.addEventListener("error", this.errorCallback, false);
+	this.httpRequest.addEventListener("error", function(){this.errorCallback("Unable to access the server.");}, false);
 	this.httpRequest.open('GET', URL);
 	this.httpRequest.send();
 }
@@ -46,23 +54,31 @@ function updateHistoricalData()
 	updateHistoryGraph(document.querySelector("history-card").shadowRoot, currentBuilding);
 	setTimeout(updateHistoricalData, 1000*60*60);
 }
-function loadError()
+
+/*
+ * Displays a 
+ */
+function showError(message)
 {
 	try
-	{document.querySelector('#ajaxError').show();}
-	catch(error)
-	{;}
+	{
+		var elem = document.querySelector('#ajaxError');
+		elem.innerHTML = message;
+		elem.show();
+	}
+	catch (error){;}
 }
 String.prototype.capitalize = function() {
 	return this.charAt(0).toUpperCase() + this.slice(1);
 }
 function updateLiveChart(livecard, buildingID)
 {
+	var name = buildingNameMap[buildingID];
 	// Request data
 	new JSONHttpRequest(
 		'api.php?building=' + buildingID + '&live=true',
-		function (result) {drawLiveChart(result, livecard);},
-		loadError
+		function (result) {drawLiveChart(result, livecard, name);},
+		showError
 	);
 }
 function updateHistoryGraph(livecard, buildingID)
@@ -71,10 +87,10 @@ function updateHistoryGraph(livecard, buildingID)
 	new JSONHttpRequest(
 		'api.php?building=' + buildingID,
 		function (result) {drawHistoryGraph(result, livecard);},
-		loadError
+		showError
 	);
 }
-function drawLiveChart(jsonResult, livecard)
+function drawLiveChart(jsonResult, livecard, buildingName)
 {
 	// Helper Functions
 	function buildingPowerPercent()
@@ -109,12 +125,13 @@ function drawLiveChart(jsonResult, livecard)
 	};
 	livecard.getElementById("amount").innerHTML = jsonResult[energyType]['amount'];
 	livecard.getElementById("units").innerHTML = jsonResult[energyType]['nativeUnit'];
-	livecard.getElementById("building").innerHTML = jsonResult['name'];
+	livecard.getElementById("building").innerHTML = buildingName;
 	livecard.getElementById("title").innerHTML = "Current " + energyType.capitalize() + " Usage";
 	var chart = new google.visualization.PieChart(livecard.getElementById('livechart'));
 	chart.draw(data, options);
 }
-function drawHistoryGraph(jsonResult, historycard) {
+function drawHistoryGraph(jsonResult, historycard)
+{
 	if (jsonResult == {} || !jsonResult['data'][energyType][historyType])
 		return;
 
